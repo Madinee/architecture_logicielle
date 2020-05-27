60a1201ad1f040670888a469316160040bd1b1d1
Madinee <madinevasimons@gmail.com>
2020-05-27 16:06:23 +0200

    add garanted class

---
+++ MaaApp/src/main/java/com/ensim/MaaApp/Controller/GarantieController.java
@@ -0,0 +1,5 @@
+package com.ensim.MaaApp.Controller;
+
+public class GarantieController {
+
+}
---
+++ MaaApp/src/main/java/com/ensim/MaaApp/Model/Garantie.java
@@ -0,0 +1,39 @@
+package com.ensim.MaaApp.Model;
+
+public class Garantie {
+	private int id;
+	private String nom;
+	private int montant;
+	private String description;
+	
+	public Garantie(String nom, int montant, String description) {
+		this.nom = nom;
+		this.montant = montant;
+		this.description = description;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getMontant() {
+		return montant;
+	}
+	public void setMontant(int montant) {
+		this.montant = montant;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
