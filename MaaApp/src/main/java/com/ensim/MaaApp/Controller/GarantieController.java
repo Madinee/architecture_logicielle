c8dc661bdae8dad03878decb48db231d957b23d1
Madinee <madinevasimons@gmail.com>
2020-05-27 17:30:12 +0200

    get, delete, put, getid finish

--- MaaApp/src/main/java/com/ensim/MaaApp/Controller/GarantieController.java
+++ MaaApp/src/main/java/com/ensim/MaaApp/Controller/GarantieController.java
@@ -1,5 +1,93 @@
 package com.ensim.MaaApp.Controller;
 
-public class GarantieController {
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.ensim.MaaApp.Model.Garantie;
+
+
+
+@RestController
+public class GarantieController {
+    private Map<Integer, Garantie> listDb = new HashMap<Integer, Garantie>();
+    
+    
+    //recuperer la liste des garanties  
+    @GetMapping("/api/garantie")
+    public ResponseEntity<Collection<Garantie>> getGarantie() {
+        return ResponseEntity.ok().body(listDb.values());
+    }
+    
+    //supprimer garantie
+    @DeleteMapping("/api/garantie/{id}")
+    public ResponseEntity<String> deleteGarantie(@PathVariable("id") @NonNull int id) {
+        if (listDb.containsKey(id)) {
+        	listDb.remove(id);
+            return ResponseEntity.ok().body(listDb.get(id).getNom()+ " a été supprimée");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+    //modifier garantie
+    @PutMapping(path = "/api/garantie/{id}", produces= {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Garantie> modifierGarantie(@PathVariable("id") @NonNull int id, @RequestBody String nom, @RequestBody String description, @RequestBody int montant) {
+        if (listDb.containsKey(id)) {
+        	Garantie garantie=listDb.get(id);
+        	garantie.setNom(nom);
+        	garantie.setMontant(montant);
+        	garantie.setDescription(description);
+        	
+        	listDb.put(id, garantie);
+        	
+            return ResponseEntity.ok(garantie);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+    
+    //recuperation de quarantie specifique
+    @GetMapping("/api/garantie/{id}")
+    public ResponseEntity<Garantie> getEquipe(@PathVariable("id") @NonNull int id) {
+        if (listDb.containsKey(id)) {
+            return ResponseEntity.ok(listDb.get(id));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+    
+    //creation de garantie
+//    @PostMapping("/api/garantie")
+//    public ResponseEntity<Garantie> postEquipe(@RequestParam("nom") String nom, @RequestParam("montant") int montant, @RequestParam("description") String description) {
+//        if (StringUtils.isBlank(nom)) {
+//            return ResponseEntity.status(400).build();
+//        }
+//
+//        // affectation d'un id et persistance
+//        Garantie garantie = new Garantie();
+//        equipe.setId(id);
+//        equipe.setName(name);
+//        fakeDb.put(equipe.getId(), equipe);
+//
+//        // URI de localisation de la ressource
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").build(equipe.getId());
+//
+//        // réponse 201 avec la localisation et la ressource créée
+//        return ResponseEntity.created(location).body(garantie);
+//    }
 }
