02abe7bb06664e70ad13bde991d28061fed07557
Madinee <madinevasimons@gmail.com>
2020-05-27 15:45:09 +0200

    first commit

---
+++ MaaApp/src/main/java/com/ensim/MaaApp/MaaAppApplication.java
@@ -0,0 +1,13 @@
+package com.ensim.MaaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaAppApplication.class, args);
+	}
+
+}
---
+++ MaaApp/src/test/java/com/ensim/MaaApp/MaaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensim.MaaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
